c4fdd5804a95cb5119f36a00750420b05bfbcf67
Gleb <kgm20011505@mail.ru>
2022-01-23 20:02:09 +0300

    1 block <header> (1/2) is done

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    //header_slider:
+    $('.header__slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 400,
+        fade: true,
+        cssEase: 'linear'
+      });
+
+
+});
